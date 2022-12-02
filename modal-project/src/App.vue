c44d34f71ec465530931cb77ae82cef32f97f6ba
Tpa Chhangte <thanpuia46@gmail.com>
2022-12-02 15:54:11 +0530

    2 nd

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Hoho</h1>
+<p>{{ title }}</p>
+<input type="text" ref="name">
+<button @click="handleClick">clik</button>
+
+<div>
+
+  <Modal header="Sign up yo" :text="lukake" theme="sale"/>
+</div>
+
+<button @click="toggleModal">Open Modal</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    //HelloWorld
+    Modal
+     },
+  data(){
+    return {
+      title:"pop",
+      header:"Sign up",
+      text:"Offer",
+      showModal:false,
+
+
+    }
+  },
+
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  },
 }
 </script>
 
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,51 @@
+ <template>
+ <div>
+    <h2>{{header}}</h2>
+    <h2>{{text}}</h2>
+
+        <div class="backdrop">
+            <div class="modal">
+                <p>modal content</p>
+                <h1>Deeper</h1>
+            </div>
+        </div>
+</div>
+
+ </template>
+<script>
+export default {
+    
+    props:['header','text', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    },
+    data(){}
+}
+</script>
+
+<style>
+    .modal {
+        width : 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        top:0;
+        position: fixed;
+        background: rgba(0, 0, 0, 5);
+        width: 100%;
+        height: 100%;
+    }
+
+    .modal h1 {
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+
+    }
+</style>
